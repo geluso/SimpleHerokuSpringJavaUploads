c7913efcbed73da8298c90afde9805d20e92ba49
Steve Geluso <stevegeluso@gmail.com>
2018-08-03 13:25:51 -0700

    init

---
+++ src/main/java/server/Application.java
@@ -0,0 +1,62 @@
+package server;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.util.FileCopyUtils;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.multipart.MultipartFile;
+
+import java.io.File;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.stream.Collectors;
+
+@Controller
+@SpringBootApplication
+public class Application {
+    public static final String UPLOAD_DIR = "src/main/resources/public/uploads";
+
+    public static void main(String[] args) {
+        SpringApplication.run((Application.class));
+        System.out.println("http://localhost:8080");
+    }
+
+    @GetMapping("/")
+    public String uploadForm(Model model) {
+        Path dir = Paths.get(UPLOAD_DIR);
+
+        List<String> files = new ArrayList<>();
+        try {
+            files = Files.walk(dir, 1)
+                    .map(path -> dir.relativize(path).toString())
+                    .filter(path -> !path.equals(""))
+                    .collect(Collectors.toList());
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+        model.addAttribute("files", files);
+        return "index";
+    }
+
+    @PostMapping("/")
+    public String upload(
+            @RequestParam("file") MultipartFile file
+    ) {
+        try {
+            String path = UPLOAD_DIR + "/" + file.getOriginalFilename();
+            FileCopyUtils.copy(file.getBytes(), new File(path));
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        return "redirect:/";
+    }
+}
